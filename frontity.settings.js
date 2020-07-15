b6e53b316eb5287b54d452865edc66ef32139111
Shaun Knowles <sknow@skmbp1.home>
2020-07-15 16:36:28 +0100

    add cpt config

--- frontity.settings.js
+++ frontity.settings.js
@@ -22,8 +22,9 @@ const settings = {
             ["A Nice Log", "/blog"]
           ],
           "featured": {
-            "showOnList": false,
-            "showOnPost": false
+            "showOnList": true,
+            "showOnPost": true,
+            "showOnPage": true,
           }
         }
       }
@@ -35,6 +36,32 @@ const settings = {
           "api": "https://primitive.press/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
           "homepage": "/home/", // optional: set when using a page as the site homepage
           "postsPage": "/blog/", // optional: set when using a page as the site homepage
+
+          "postTypes": [
+            {
+              "type": "works", // custom post type slug
+              "endpoint": "works", // REST API endpoint
+              "archive": "/works" // DO I NEED THIS???
+            },
+            {
+              "type": "temporal_events", // custom post type slug
+              "endpoint": "temporal_events", // REST API endpoint
+              "archive": "/evolution-of-digital-stuff" // link where this custom posts are listed
+            },
+            {
+              "type": "things", // custom post type slug
+              "endpoint": "things", // REST API endpoint
+              "archive": "/things" // link where this custom posts are listed
+            }
+          ],
+          "taxonomies": [
+            {
+              "taxonomy": "timelines", // custom taxonomy slug
+              "endpoint": "timelines", // REST API endpoint
+              "postTypeEndpoint": "/temporal_events", // endpoint from which posts from this taxonomy are fetched
+            }
+          ]
+          
         }
 
       }
