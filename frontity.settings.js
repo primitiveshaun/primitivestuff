805f0748e5af2157025b9fc8c74df21c572c3b49
Shaun Knowles <sknow@skmbp1.home>
2020-08-11 08:18:01 +0100

    tests: ssl chaining error on api.

--- frontity.settings.js
+++ frontity.settings.js
@@ -33,7 +33,8 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "api": "http://api.primitivedigital.uk/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
+          "api": "https://primitive.press/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
+          //"api": "https://api.primitivedigital.uk/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
           "homepage": "/home/", // optional: set when using a page as the site homepage
           "postsPage": "/blog/", // optional: set when using a page as the site homepage
 
