348773bfbf409fb21eaac71a373e5a4f8ac50fa9
Shaun Knowles <sknow@skmbp1.home>
2020-08-20 14:26:21 +0100

    junk

--- build/bundling/entry-points/primitivestuff/client.ts
+++ build/bundling/entry-points/primitivestuff/client.ts
@@ -17,34 +17,3 @@ const packages = {
 
 export default client({ packages });
 
-if (module["hot"]) {
-  module["hot"].accept(
-    [
-      "@frontity/core/src/client",
-      "primitive-theme/src/index",
-      "@frontity/wp-source/src/index",
-      "@frontity/tiny-router/src/index",
-      "@frontity/html2react/src/index",
-      "@frontity/head-tags/src/index",
-      "frontity-contact-form-7/src/index",
-    ],
-    () => {
-      const client = require("@frontity/core/src/client").default;
-      const primitive_theme_default = require("primitive-theme/src/index").default;
-      const frontity__wp_source_default = require("@frontity/wp-source/src/index").default;
-      const frontity__tiny_router_default = require("@frontity/tiny-router/src/index").default;
-      const frontity__html2react_default = require("@frontity/html2react/src/index").default;
-      const frontity__head_tags_default = require("@frontity/head-tags/src/index").default;
-      const frontity_contact_form_7_default = require("frontity-contact-form-7/src/index").default;
-      const packages = {
-        primitive_theme_default,
-        frontity__wp_source_default,
-        frontity__tiny_router_default,
-        frontity__html2react_default,
-        frontity__head_tags_default,
-        frontity_contact_form_7_default,
-      };
-      client({ packages });
-    }
-  );
-}
