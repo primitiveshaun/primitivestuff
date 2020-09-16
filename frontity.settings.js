66fc859331d69e39ce371aa8f035470a3080c302
Shaun Knowles <sknow@skmbp1.home>
2020-09-16 14:57:22 +0100

    remove encapsulation for readability

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,19 +1,19 @@
 const settings = {
-  "name": "primitivestuff",
-  "state": {
-    "frontity": {
-      "url": "https://dev.primitivedigital.uk",
-      "title": "Makin' a Website?",
-      "description": "WordPress + Frontity Stuff",
-      "name": "Primitive Digital"
+  name: "primitivestuff",
+  state: {
+    frontity: {
+      url: "https://dev.primitivedigital.uk",
+      title: "Makin' a Website?",
+      description: "WordPress + Frontity Stuff",
+      name: "Primitive Digital"
     }
   },
-  "packages": [
+  packages: [
     {
-      "name": "primitive-theme",
-      "state": {
-        "theme": {
-          "menu": [
+      name: "primitive-theme",
+      state: {
+        theme: {
+          menu: [
             ["Testing", "/stats"],
             ["Pages", "/web-design"],
             ["Development", "/frontity-tests"],
@@ -21,51 +21,53 @@ const settings = {
             ["Content", "/blog/a-decade-of-digital-development/"],
             ["Blog", "/blog"]
           ],
-          "featured": {
+          featured: {
             "showOnList": true,
             "showOnPost": true,
             "showOnPage": true,
           },
           // tbc
-          "archive": {
+          archive: {
             "showExcerpt": false
-          }      
+          }
         }
       }
     },
     {
-      "name": "@frontity/wp-source",
-      "state": {
-        "source": {
-          "api": "https://api.primitivedigital.uk/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
-          "homepage": "/home/", // optional: set when using a page as the site homepage
-          "postsPage": "/blog/", // optional: set when using a page as the site homepage
+      name: "@frontity/wp-source",
+      state: {
+        source: {
+          api: "https://api.primitivedigital.uk/wp-json", // required: Your WP REST API EndPoint (no trailing slash)
+          
+          homepage: "/home/", // optional: set when using a page as the site homepage (wp hp settings ignored)
+          postsPage: "/blog/", // optional: set when using a page as the site homepage
 
-          "postTypes": [
+          // optional: define CPTs
+          postTypes: [
             {
-              "type": "works", // custom post type slug
-              "endpoint": "works", // REST API endpoint
-              "archive": "/works" // DO I NEED THIS???
+              type: "works", // custom post type slug
+              endpoint: "works", // REST API endpoint
+              archive: "/works" // DO I NEED THIS???
             },
             {
-              "type": "temporal_events", // custom post type slug
-              "endpoint": "temporal_events", // REST API endpoint
-              "archive": "/evolution-of-digital-stuff" // link where this custom posts are listed
+              type: "temporal_events", // custom post type slug
+              endpoint: "temporal_events", // REST API endpoint
+              archive: "/evolution-of-digital-stuff" // link where this custom posts are listed
             }
             /*
             ,
             {
-              "type": "things", // custom post type slug
-              "endpoint": "things", // REST API endpoint
-              "archive": "/things" // link where this custom posts are listed
+              type: "things", // custom post type slug
+              endpoint: "things", // REST API endpoint
+              archive: "/things" // link where this custom posts are listed
             }
             */
           ],
-          "taxonomies": [
+          taxonomies: [
             {
-              "taxonomy": "timelines", // custom taxonomy slug
-              "endpoint": "timelines", // REST API endpoint
-              "postTypeEndpoint": "/temporal_events", // endpoint from which posts from this taxonomy are fetched
+              taxonomy: "timelines", // custom taxonomy slug
+              endpoint: "timelines", // REST API endpoint
+              postTypeEndpoint: "/temporal_events", // endpoint from which posts from this taxonomy are fetched
             }
           ]
         }
@@ -73,7 +75,8 @@ const settings = {
     },
     "@frontity/tiny-router",
     "@frontity/html2react",
-    "@frontity/head-tags",
+    //"@frontity/head-tags",
+    "@frontity/yoast",
     "frontity-contact-form-7"
   ]
 };
