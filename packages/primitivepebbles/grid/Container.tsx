5ac79e7a023cea94358dca1181ecabcefe670ab5
Shaun Knowles <shaun@primitivedigital.uk>
2022-01-16 12:10:15 +0000

    tests

--- packages/primitivepebbles/grid/Container.tsx
+++ packages/primitivepebbles/grid/Container.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect, styled } from "frontity";
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
@@ -46,7 +47,7 @@ const Container: Container = React.forwardRef(
       bsPrefix,
       fluid,
       // Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
-      as: Component = 'div',
+      as: Component = StyledComponent,
       className,
       ...props
     }: ContainerProps,
@@ -54,6 +55,7 @@ const Container: Container = React.forwardRef(
   ) => {
     const prefix = useBootstrapPrefix(bsPrefix, 'container');
     const suffix = typeof fluid === 'string' ? `-${fluid}` : '-fluid';
+
     return (
       <Component
         ref={ref}
@@ -68,7 +70,38 @@ Container.displayName = 'Container';
 Container.propTypes = propTypes;
 Container.defaultProps = defaultProps;
 
-export default Container;
+export default connect(Container);
+
+const StyledComponent = styled.div`
+
+width: 100%;
+padding-right: 1rem;
+padding-left: 1rem;
+margin-right: auto;
+margin-left: auto;
+
+
+
+  /* --- RESPONSIVE --- */
+  /* Small devices (landscape phones, 576px and up) */
+  /* Medium devices (tablets, 768px and up)  */
+  /* Large devices (desktops, 992px and up) */
+  /* Extra large devices (large desktops, 1200px and up) */
+
+  @media screen and (min-width: 576px) {
+    .container { max-width: 540px; }
+  }
+  @media screen and (min-width: 768px) {
+    .container { max-width: 720px; }
+  }
+  @media screen and (min-width: 992px) {
+    .container { max-width: 960px; }
+  }
+  @media screen and (min-width: 1200px) {
+    .container { max-width: 1140px; }
+  }
+
+`;
 
 // Based on: 
 // https://github.com/react-bootstrap/react-bootstrap/blob/master/src/Container.tsx
